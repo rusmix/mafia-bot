abcefb4ba8307bca4333d8526f94a5590cfb9da4
rusmix <r.m.halilulin@gmail.com>
2022-11-02 17:44:10 +0300

    init commit 2

--- src/handlers/showPlayers.ts
+++ src/handlers/showPlayers.ts
@@ -1,14 +1,14 @@
-import bot from '@/helpers/bot'
-import { eventsKeyboard } from '@/helpers/keyboards'
-import Context from '@/models/Context'
-import { EventModel } from '@/models/Event'
+// import bot from '@/helpers/bot'
+// import { eventsKeyboard } from '@/helpers/keyboards'
+// import Context from '@/models/Context'
+// import { EventModel } from '@/models/Event'
 
-export default async function showPlayers(ctx: Context) {
-  const media = []
-  const event = await EventModel.findOne({ title: ctx.session.currentTitle })
-  const players = event.players
+// export default async function showPlayers(ctx: Context) {
+//   const media = []
+//   const event = await EventModel.findOne({ title: ctx.session.currentTitle })
+//   const players = event.players
 
-  await bot.api.sendMediaGroup(ctx.from.id, {
-    type: 'photo',
-  })
-}
+//   await bot.api.sendMediaGroup(ctx.from.id, {
+//     type: 'photo',
+//   })
+// }
