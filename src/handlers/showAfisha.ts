6526fd9dad8bbf5dd5a5c096044a3724ca0f73a5
rusmix <r.m.halilulin@gmail.com>
2022-11-02 19:54:59 +0300

    minor afisha update

--- src/handlers/showAfisha.ts
+++ src/handlers/showAfisha.ts
@@ -1,15 +1,21 @@
 import { eventsKeyboard } from '@/helpers/keyboards'
 import Context from '@/models/Context'
+import { EventModel } from '@/models/Event'
 
 export default async function showAfisha(ctx: Context) {
   // await ctx.reply('Доступные для записи мероприятия!', {
   //   reply_markup: await eventsKeyboard(),
   // })
-  await ctx.replyWithPhoto(
-    'AgACAgIAAxkBAAISMmNgRNqRDhx8mM40sDVAPRV8zAN4AAIdwTEbwWMBS4eDxO1p5mPRAQADAgADeAADKgQ',
-    {
-      caption: 'Доступные для записи мероприятия!',
-      reply_markup: await eventsKeyboard(),
-    }
-  )
+  const events = await EventModel.find({ isActual: true })
+  if (!events)
+    return await ctx.reply('На данный момент никаких событий не запланировано!')
+
+  events.sort(function (a, b) {
+    return +new Date(a.date) - +new Date(b.date)
+  })
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  await ctx.replyWithPhoto(events[0].photoId, {
+    caption: 'Доступные для записи мероприятия!',
+    reply_markup: await eventsKeyboard(),
+  })
 }
