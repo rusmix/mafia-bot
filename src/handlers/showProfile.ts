950bab9876630f35da5bc54c2848c8652198f499
rusmix <r.m.halilulin@gmail.com>
2022-11-08 14:56:19 +0300

    show profile bugfix

--- src/handlers/showProfile.ts
+++ src/handlers/showProfile.ts
@@ -5,6 +5,10 @@ import { EventModel } from '@/models/Event'
 export default async function registerToEvent(ctx: Context) {
   const user = ctx.dbuser
   console.log(user)
+  if (!user.photoId)
+    return await ctx.reply(
+      `Профиль\n\nИмя: ${user.name}\nТелефон: ${user.phone}\nКоличество бонусных баллов: ${user.balance}\n\nСтатистика: ${user.thisMonthStats.gamesTotal} игр сыграно.`
+    )
   await ctx.replyWithPhoto(`${user.photoId}`, {
     caption: `Профиль\n\nИмя: ${user.name}\nТелефон: ${user.phone}\nКоличество бонусных баллов: ${user.balance}\n\nСтатистика: ${user.thisMonthStats.gamesTotal} игр сыграно.`,
   })
