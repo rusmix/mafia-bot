1ee7c6f15ebb3eea7c39017a2b480d87f76f9649
rusmix <r.m.halilulin@gmail.com>
2023-05-15 23:22:39 +0300

    before eject

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,7 +1,7 @@
 const formatDate = (str: string): Date | undefined => {
   const [year, month, date, hours, minutes] = str
     .split('-')
-    .map((el) => Number(el))
+    .map((el) => Number(el));
   console.log(
     'year: ',
     year,
@@ -11,11 +11,11 @@ const formatDate = (str: string): Date | undefined => {
     date,
     'minutes: ',
     minutes
-  )
-  const formattedDate = new Date(year, month - 1, date, hours, minutes, 0, 0)
-  console.log(formattedDate)
-  if (!formattedDate) return
-  return formattedDate
-}
+  );
+  const formattedDate = new Date(year, month - 1, date, hours, minutes, 0, 0);
+  console.log(formattedDate);
+  if (!formattedDate) return;
+  return formattedDate;
+};
 
-export default formatDate
+export default formatDate;
